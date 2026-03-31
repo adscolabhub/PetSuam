873e5dbb80a8450b782e9940159bd91222ea380a
ItzIgor <silva.igor.014@gmail.com>
2026-03-31 08:59:44 -0300

    atualização

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,20 @@
-import { TextInput, View, Text, StyleSheet} from 'react-native';
+import { TextInput, View, Text, StyleSheet, Button,Linking, TouchableOpacity} from 'react-native';
 
-export default function Form({h1, h2, h3}) {
+export default function Form({h1, h2, h3, children, btnPlaceholder}) {
     return (
-        <>
+        <View style={styles.formContainer}>
             <Text style={styles.h1}>{h1}</Text>
             <Text style={styles.h2}>{h2}</Text>
             <Text style={styles.h3}>{h3}</Text>
-        </>
+
+            <View>
+              {children}
+
+              <View style={styles.buttonContainer}>
+                <Button  title={btnPlaceholder}/>
+              </View>
+            </View>
+        </View>
 
     );
 }
@@ -15,11 +23,23 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 10,
+    color: "#083068"
   },
   h2: {
     fontSize: 20,
+    marginBottom: 5,
+    color: "#C8DBEF"
   },
   h3: {
-    fontSize: 18
+    fontSize: 18,
+    color: "#C8DBEF"
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+  formContainer: {
   }
+
+
 });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Form from "../components/Form.js";
+import Input from "../components/Input.js";
 
 export default function Login() {
   return (
@@ -10,7 +11,23 @@ export default function Login() {
         h1="PetSuam"
         h2="Login"
         h3="Bem-vindos ao PetSuam"
-      />
+        btnPlaceholder="Enviar"
+      >
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+        <Input
+          placeholder="Senha"
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+
+      </Form>
 
       <StatusBar style="auto" />
     </View>
